856090d141b8c7261974aabd0c56ca74b9b313be
jaxon <jaxonmoh05@gmail.com>
2026-01-31 18:42:50 +0800

    frontend predeployment config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/reactjs-template/',
+    base: '/nusc-queuebot/',
     css: {
         preprocessorOptions: {
             scss: {
