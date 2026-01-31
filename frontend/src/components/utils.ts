c4d3e0c236fccd3416befaf1e0a4123a55388dc6
jaxon <jaxonmoh05@gmail.com>
2026-01-31 21:40:57 +0800

    fix missing css

--- frontend/src/components/utils.ts
+++ frontend/src/components/utils.ts
@@ -1,5 +1,5 @@
 const BACKEND_DEV_URL = 'http://localhost:3000';
-const BACKEND_PROD_URL = 'https://fastify-test-backend-qblzvwa6v-jaxons-projects-5f854f7a.vercel.app';
+const BACKEND_PROD_URL = 'https://fastify-test-backend.vercel.app';
 
 export function createPath(path: string) {
   return `${import.meta.env.DEV ? BACKEND_DEV_URL : BACKEND_PROD_URL}/${path}`;
