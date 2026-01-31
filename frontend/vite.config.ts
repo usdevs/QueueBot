6c663759dc4d8947adc8555ace879c013858a5c3
jaxon <jaxonmoh05@gmail.com>
2026-01-31 21:13:47 +0800

    fix vite config error

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/frontend/',
+    base: '/',
     css: {
         preprocessorOptions: {
             scss: {
