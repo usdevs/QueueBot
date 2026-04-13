bc189e8982881154bb590bcddfa6e6adbf4ca1df
jaxon <jaxonmoh05@gmail.com>
2026-04-13 15:52:44 +0800

    enable telegram notification in backend

--- backend/src/routes/private/queue/next.ts
+++ backend/src/routes/private/queue/next.ts
@@ -30,10 +30,10 @@ const route: FastifyPluginAsyncZod = async (fastify, _) => {
             }
             const queryString = new URLSearchParams(
                 {'chat_id': allEntries[i]!.telegram_id, 'text': message, 'parse_mode': 'Markdown'}).toString();
-            // hit Telegram API to send user message
-            // fetch(`https://api.telegram.org/bot${BOT_TOKEN}/sendMessage?${queryString}`, {
-            //     method: 'POST',
-            // })
+            //hit Telegram API to send user message
+            fetch(`https://api.telegram.org/bot${BOT_TOKEN}/sendMessage?${queryString}`, {
+                method: 'POST',
+            })
         }
 
         return reply.code(200).send({entries: allEntries});
